a4210854a0da194c6cdce04e834fabc1bf49a8ab
Irina Uleva <irinauleva2@gmail.com>
2025-03-21 00:18:18 +0300

    дада

---
+++ src/main/java/ru/tbank/fdsspring/service/CurrencyService.java
@@ -0,0 +1,39 @@
+package ru.tbank.fdsspring.service;
+
+import org.springframework.stereotype.Service;
+import ru.tbank.fdsspring.Currency;
+import ru.tbank.fdsspring.CurrencyRepository;
+
+import java.util.List;
+
+@Service
+public class CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+
+    public CurrencyService(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    public List<Currency> getAllCurrencies() {
+        return currencyRepository.findAll();
+    }
+
+    public Currency addCurrency(Currency currency) {
+        return currencyRepository.save(currency);
+    }
+
+    public Currency getCurrencyById(String id) {
+        return currencyRepository.findById(id).orElseThrow(() -> new RuntimeException("Валюта не найдена"));
+    }
+
+    public Currency updateCurrency(String id, Currency currency) {
+        currency.setId(id);
+        return currencyRepository.save(currency);
+    }
+
+    public void deleteCurrency(String id) {
+        currencyRepository.deleteById(id);
+    }
+}
+//типо изменение
